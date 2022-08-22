a559d81ca2ee8555bc599bd72e2fbee909df5abb
themodernmonk7 <sauravavishek@gmail.com>
2022-08-22 10:44:15 +0530

    Custom Error setup

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,14 +1,9 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { StatusCodes } = require("http-status-codes")
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
     // set default
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || "Something went wrong try again later",
-  }
-
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
   }
 
   // Duplicate Error
@@ -20,7 +15,6 @@ const errorHandlerMiddleware = (err, req, res, next) => {
   }
   // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({err})
   return res.status(customError.statusCode).json({ msg: customError.msg })
-
 }
 
 module.exports = errorHandlerMiddleware
