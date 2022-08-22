3eecfdf36de207857dd35cf3d9fb928e4009334c
themodernmonk7 <sauravavishek@gmail.com>
2022-08-22 07:36:02 +0530

    Setup jobController.js

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,24 +1,24 @@
-// Get all jobs
+//* ========================= GET ALL JOBS ========================= */
 const getAllJobs = async (req, res) => {
   res.send("Get all jobs")
 }
 
-// Get single job
+//* ========================= GET SINGLE JOB ========================= */
 const getSingleJob = async (req, res) => {
   res.send("Get single job")
 }
 
-// Create job
+//* ========================= CREATE JOB ========================= */
 const createJob = async (req, res) => {
   res.send("Create job")
 }
 
-// Update job
+//* ========================= UPDATE JOB ========================= */
 const updateJob = async (req, res) => {
   res.send("Update job")
 }
 
-// Delete job
+//* ========================= DELETE JOB ========================= */
 const deleteJob = async (req, res) => {
   res.send("Delete job")
 }
